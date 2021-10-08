ce40294ff502aec841d38783b11a32ee0b6e3db0
Pedro Barbosa <pedro_dith@hotmail.com>
2021-10-07 22:32:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function countWeeks() {
+   return (
+      data.map(currentData => (
+         <th></th>
+      ))
+   )
 }
+
+function tableData() {
+   return (
+      data.map(currentData => (
+         <tr>
+            <td>{currentData.actName}</td>
+            <td>{currentData.color}</td>
+         </tr>
+      ))
+   )
+}
+
+function App() {
+   return (
+      <div className="App">
+         <table>
+            <tr>
+               <th>Atividade</th>
+               {
+                  countWeeks()
+               }
+               <th>Ações</th>
+            </tr>
+            {
+               tableData()
+            }
+         </table>
+      </div>
+   );
+}
+
+const data = [
+   {
+      id: 1,
+      actName: 'Activity 1',
+      weeksOn: [
+         {
+            wk: 1,
+            percent: 20
+         },
+         {
+            wk: 2,
+            percent: 50
+         }
+      ],
+      color: 'blue'
+   },
+   {
+      id: 2,
+      actName: 'Activity 2',
+      weeksOn: [
+         {
+            wk: 1,
+            percent: 60
+         },
+         {
+            wk: 2,
+            percent: 50
+         }
+      ],
+      color: 'red'
+   },
+]
 
 export default App;
